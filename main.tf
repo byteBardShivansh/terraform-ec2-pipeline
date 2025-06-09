de1f23179b2d174eaa619a25ad6ea52cde44c344
Shivansh Mishra <autocodebuddy@gmail.com>
2025-06-09 18:31:11 +0530

    Removed spaces

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"  # Specify the AWS region where you want to deploy your instance
+  region = "us-east-1" # Specify the AWS region where you want to deploy your instance
 }
 
 resource "aws_instance" "web-instance" {
-  ami           = "ami-014d544cfef21b42d"  # Amazon Linux 2 AMI ID (update for your region)
-  instance_type = "t2.micro"               # EC2 instance type
+  ami           = "ami-014d544cfef21b42d" # Amazon Linux 2 AMI ID (update for your region)
+  instance_type = "t2.micro"              # EC2 instance type
 
   tags = {
     Name = "Nginx-Instance"
