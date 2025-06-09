f960de8f2c40694ee6724d6e1e02b33436ea6343
Shivansh Mishra <autocodebuddy@gmail.com>
2025-06-09 18:20:33 +0530

    Updated AMI id for Amazon Linux 2

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web-instance" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI ID (update for your region)
+  ami           = "ami-014d544cfef21b42d"  # Amazon Linux 2 AMI ID (update for your region)
   instance_type = "t2.micro"               # EC2 instance type
 
   tags = {
